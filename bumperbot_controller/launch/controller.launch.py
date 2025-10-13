c0650257475afad9e84c9575406bd6c37fab77f9
alan_khang <alan_khang@alan.luu>
2025-10-13 07:36:57 +0700

    done prob_for_robotics chapter

--- bumperbot_controller/launch/controller.launch.py
+++ bumperbot_controller/launch/controller.launch.py
@@ -1,8 +1,23 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition, UnlessCondition
+
+def noisy_controller(context, *args, **kwargs):
+    wheel_radius = float(LaunchConfiguration('wheel_radius').perform(context))
+    wheel_separation = float(LaunchConfiguration('wheel_separation').perform(context))
+    wheel_radius_error = float(LaunchConfiguration('wheel_radius_error').perform(context))
+    wheel_separation_error = float(LaunchConfiguration('wheel_separation_error').perform(context))
+
+    noisy_controller_node = Node(
+        package="bumperbot_controller",
+        executable="noisy_controller",
+        parameters=[
+            {"wheel_radius": wheel_radius + wheel_radius_error,
+             "wheel_separation": wheel_separation + wheel_separation_error}])
+
+    return [noisy_controller_node]
 
 def generate_launch_description():
 
@@ -17,6 +32,14 @@ def generate_launch_description():
     use_simple_controller_arg = DeclareLaunchArgument(
         'use_simple_controller',
         default_value="True")
+
+    wheel_radius_error_arg = DeclareLaunchArgument(
+        'wheel_radius_error',
+        default_value='0.005')
+
+    wheel_separation_error_arg = DeclareLaunchArgument(
+        'wheel_separation_error',
+        default_value='0.02')
 
     wheel_radius = LaunchConfiguration('wheel_radius')
     wheel_separation = LaunchConfiguration('wheel_separation')
@@ -56,11 +79,16 @@ def generate_launch_description():
                     "wheel_radius": wheel_radius,
                     "wheel_separation": wheel_separation}])])
 
+    noisy_controller_launch = OpaqueFunction(function=noisy_controller)
+
     return LaunchDescription([
         wheel_radius_arg,
         wheel_separation_arg,
+        wheel_radius_error_arg,
+        wheel_separation_error_arg,
         use_simple_controller_arg,
         joints_state_broadcaster_spawner,
         wheel_controller_spawner,
         simple_controller,
+        noisy_controller_launch
     ])
